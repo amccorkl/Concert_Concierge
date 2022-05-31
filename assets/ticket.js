276a8c1edc26b62bcedb891e84f00b959f07eb70
Anne McCorkle <annefmccorkle@gmail.com>
2022-05-31 12:16:33 -0600

    reworking ticketmaster js ui

--- assets/ticket.js
+++ assets/ticket.js
@@ -1,21 +1,27 @@
-const ticketmasterEl = document.getElementById("ticketmaster-results");
+const ticketmasterEl = document.getElementById("ticketmaster-search");
 //search for events
 let testCity = "Denver";
-let cityName = "";
+let cityName = document.getElementById("city-name");
+let musicChoice = document.getElementById("band-name");
 
 //can't get the event genre to work yet
-let eventGenre = "Music";
+let eventClassification = "Music";
 let eventTypeArr = ["Sports", "Music", "Arts & Theater", "Film", "Miscellaneous"];
 const ticketMasterApiKey = "n98GKJ3ZAswvAkjGcvK9zMAoAj0ppMD8";
 
 
-function userChoices() {
+let ticketMasterSearch = function userChoices() {
+    
     //the "events" parameter allows for a search by location, date, availability" not sure if we should include a calendar before asking for the user to input a city name
 
-    let cityInput = cityName.trim();
+    let cityInput = cityName.value;
+    let bandInput = musicChoice.value;
     
-    let ticketMasterRequestUrl = `https://app.ticketmaster.com/discovery/v2/events.json?city=${cityInput}&size=5&sort=date,name,asc&includeSpellcheck&apikey=${ticketMasterApiKey}`
+    let ticketMasterRequestUrl = `https://app.ticketmaster.com/discovery/v2/events.json?city=${testCity}&keyword=${bandInput}&radius=30&sort=date,name,asc&includeSpellcheck&apikey=${ticketMasterApiKey}`
+    // &size=5
+    //${segmentName} - eventTypeArr
 
+    
     fetch(ticketMasterRequestUrl)
         .then(function (response) {
             return response.json();
@@ -23,48 +29,56 @@ function userChoices() {
         .then(function(data) {
             console.log(data);
             
+
             //shorted variable for the info so I don't have to type it all out every time
-            let events = data._embedded.events
+            let eventList = data._embedded.events;
 
-            for (let i = 0; i < events.length; i++) {
-                let listOfEvents = events[i];
-                let eventName = listOfEvents.name;               
-                let eventDate = listOfEvents.dates.start.localDate;
-                //added moment()connection here once link is in
-                // eventDate = moment("YYYY-MM-DD").format("MMMM Do, YYYY");
-                // let eventTime = listOfEvents.dates.localTime;
-               
-                let eventImages = listOfEvents.images[0].url; //image not showing up yet on UI
-                let eventUrl = listOfEvents.url; //doesn't link yet
-                // let description = listOfEvents.description; - it is a really long description
+            for (let i = 0; i < eventList.length; i++) {
 
-                console.log(eventName, eventDate, eventImages, eventUrl);
+                let event = eventList[i];
+                let eventName = event.name;               
+                let eventDate = event.dates.start.localDate;
+                eventDate = moment(eventDate, "YYYY-MM-DD").format("MMMM Do, YYYY");
+                let eventTime = event.dates.start.localTime.hourOfDay;
+
+                let eventImages = event.images[0].url; 
+                let eventUrl = event.url; //doesn't link yet
+                let eventVenue = event._embedded.venues[0].name;
+
+                // if we want user to choose what type of event to look for   
+                // let genreOfEvent = event.classifications.segment.genre;
+                
+                
+                console.log("event: ", eventName, "image: ", eventImages,  "date: ", eventDate, "time: ", eventTime, "link: ", eventUrl, "venue: ", eventVenue);
+                
                                 
 
                 //get the UI element that the ticketMaster info will go into
                 let ticketMasterDiv = document.createElement('div');
                 let eventTitle = document.createElement('h3');
-                let dateEl = document.createElement('p');
-                // let timeEl = document.createElement('p');
-                let imagesEl = document.createElement('img');
-                let buyNow = document.createElement('h5');
+                // let dateEl = document.createElement('p');
+                let dateEl = document.createElement('p');  
+                let timeEl = document.createElement('p')              
                 let urlEl = document.createElement('a');
-
+                let venueEl = document.createElement('p');
+                
+                
                 eventTitle.textContent = eventName;
                 dateEl.textContent = eventDate;
-                // timeEl.textContent = eventTime;
-                imagesEl.setAttribute("src", eventImages);
-                imagesEl.setAttribute("style", "width: 50%; height: 50%;");
-                buyNow.textContent = "Click on the link below to purchase tickets!";
-                urlEl.textContent = eventUrl;
+                timeEl.textContent = " Start Time: " + eventTime;
+                urlEl.textContent = "Click here to buy tickets!";
                 urlEl.setAttribute("src", eventUrl);
                 console.log(eventUrl);
+                venueEl.textContent = eventVenue;
 
-                ticketMasterDiv.append(eventTitle, dateEl, imagesEl, buyNow, urlEl);
+
+                let imagesEl = document.createElement('img');
+                imagesEl.setAttribute("src", eventImages);
+                imagesEl.setAttribute("style", "width: 50%; height: 50%;");
+
+                ticketMasterDiv.append(eventTitle, dateEl, timeEl, venueEl, imagesEl, urlEl);
                 ticketmasterEl.append(ticketMasterDiv);
-                
-
-                
+              
             }
         })
         // .catch((error) => {
@@ -73,4 +87,11 @@ function userChoices() {
         // });
 }
 
-userChoices();
+ticketMasterSearch();
+//open new window to show events 
+// ticketMaster.addEventListener("click", {
+//     window.open("url...", windowFeatures )
+// });
+
+// userChoices();
+
