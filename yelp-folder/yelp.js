7e04bba6d955da01b9d0a3aa3b881996f51ff056
Anne McCorkle <annefmccorkle@gmail.com>
2022-06-04 12:49:51 -0600

    working yelp api with some bits dynamically created for a the UI

--- yelp-folder/yelp.js
+++ yelp-folder/yelp.js
@@ -1,22 +1,28 @@
 let yelpContainerEl = document.getElementById("yelp-id");
-let yelpForm = document.getElementById("food-form");
-let restaurantSearch = document.getElementById("restaurant");
+let yelpSearchForm = document.getElementById("yelp-food-search");
+let restaurantSearchInput = document.getElementById("foodType");
 let searchBtn = document.getElementById("submit-btn");
-let restSearchResults = document.getElementById("restaurant-results");
-let foodHistoryEl = document.getElementById("saved-results");
+let foodSearchResultsEl = document.getElementById("restaurant-results");
+let foodHistoryEl = document.getElementById("food-saved-results");
 let testCity = "Denver";
+let cityName = document.getElementById("city-name");
+
 let testFood = "Indian";
 
 
-// let yelpQueryUrl = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search`;
 
-// let yelpRequestURL = "https://api.yelp.com/v3/businesses/search?term=food,restaurants,drink,cafes&categories=food,All&location=${testCity}" 
+let searchYelpApi = function(event) {
+    event.preventDefault();
+    console.log(event);
 
-let yelpApiKey = 'zjzCcRjFdB51w-cAx9BNvLkPUrZv7SxLzuEioFbVaIUNUvoYM8sekjD5xJUyqYlkA77que94UbQG8U5dv6OQprKQn_v-mRGul4UX0LUo5RULjXPqHWoKTeug5GSVYnYx';
+    let cityInput = cityName.value;
+    cityName = "";
 
-let proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    let yelpApiKey = 'zjzCcRjFdB51w-cAx9BNvLkPUrZv7SxLzuEioFbVaIUNUvoYM8sekjD5xJUyqYlkA77que94UbQG8U5dv6OQprKQn_v-mRGul4UX0LUo5RULjXPqHWoKTeug5GSVYnYx';
 
-let yelpQueryUrl = `https://api.yelp.com/v3/businesses/search?location=${testCity}&term=${testFood}`;
+    let proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+
+    let yelpQueryUrl = `https://api.yelp.com/v3/businesses/search?location=${testCity}&term=${testFood}`;
     //default is 20 restaurants
     //term=${testFood}
     //&radius=30&sort_by=rating
@@ -41,31 +47,37 @@ let yelpQueryUrl = `https://api.yelp.com/v3/businesses/search?location=${testCit
         .then(function(data) {
             // renderRestaurants(data);
             console.log(data);
+
+            let cardInfo = data.businesses;
+                console.log({cardInfo}); 
+
+
+            for(let i = 0; i < data.length; i++) {
+                let element = cardInfo[i];
+            }
+            let restaurantNameEl = document.createElement("h4");
+            let phoneEl = document.createElement("p");
+            let imageEl = document.createElement("img");
+            let locationEl = document.createElement("p");
+            let ratingEl = document.createElement("p");
+            let priceEl = document.createElement("p");
+            let urlEl = document.createElement("p");
+
+            let restaurantName = element.name;
+            let phone = element.phone;
+            let rating = element.rating;
+            let price = element.price;
+            let location = element.location;
+            let image = element.image_url;
+            
         });
         
+
+}
+ 
+searchBtn.addEventListener("click", searchYelpApi);       
         
-        
 
 
 
-
-
-// 
-
-//other callbacks
-// $.ajax({
-//     url: proxyUrl + yelpQueryUrl,
-//     method: 'GET',
-//     headers: {
-//         "accept": "application/json",
-//         "x-requested-with": "xmlhttprequest",
-//         "Access-Control-Allow-Origin": "*",
-//         "Authorization": `Bearer ${yelpApiKey}`
-//     }
-        
-// }).then (function (response) {
-//         return response.json();
-// }).then(function(data) {
-//         console.log(data);
-// });
 
