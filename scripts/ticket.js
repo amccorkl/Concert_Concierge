1d783c8c3fee2a764076d826e73238dcf3c411d0
Anne McCorkle <annefmccorkle@gmail.com>
2022-06-07 12:31:23 -0600

    further click on a music event or restaurant opens a new window

--- scripts/ticket.js
+++ scripts/ticket.js
@@ -9,32 +9,10 @@ let eventClassification = "Music";
 let keyword = document.getElementById("keyword");
 let savedViewsDiv = document.getElementById("saved-music-views");
 // let savedMusicArr =  JSON.parse(localStorage.getItem(savedMusic)) || [];
-
+let dateArr = [];
 
 const ticketMasterApiKey = "n98GKJ3ZAswvAkjGcvK9zMAoAj0ppMD8";
 
-// get saved music event data from localStorage
-
-// let showSavedMusic = function(savedMusicArr) {
-//     for(let i = 0; i < savedMusicArr.length; i++) {
-//         let musicGroup = savedMusicArr[i];
-//         console.log(musicGroup);
-        
-//     //create a button for each event viewed
-//     let musicGroupBtn = document.createElement("button");
-//     musicGroupBtn.setAttribute("class", "music-btn");
-//     musicGroupBtn.setAttribute("value", musicGroup);
-//     musicGroupBtn.textContent = musicGroup;
-
-//     savedViewsDiv.append(musicGroupBtn);
-
-//         musicGroupBtn.addEventListener("click", function() {
-//             console.log(this);
-//             ticketMasterSearch(this.value);
-//         })
-
-//     }
-// }
 
 let ticketMasterSearch = function (event) {
     event.preventDefault();
@@ -46,9 +24,26 @@ let ticketMasterSearch = function (event) {
     cityName.value = "";
 
     let dateInput = date.value;
+    console.log(date.value);
+    let splitDate = date.value.split(",");
+    console.log(splitDate);
+    let beforeDate = parseInt(splitDate[0].split(" ")[1]) - 1;
+    let afterDate = parseInt(splitDate[0].split(" ")[1]) + 1;
+    let yesterday = splitDate[0].split(" ")[0] + " " + beforeDate + "," + splitDate[1];
+    let tomorrow = splitDate[0].split(" ")[0] + " " + afterDate + "," + splitDate[1];
+    console.log(yesterday, tomorrow);
+
     //use moment to reformat datepicker into TM's format for parsing
     let dateMoment = moment(dateInput).utc().format();
+    let dateMomentYesterday = moment(yesterday).utc().format();
+    let dateMomenttomorrow = moment(tomorrow).utc().format();
     console.log({dateMoment});
+    console.log({dateMomentYesterday, dateMomenttomorrow});
+    dateArr.push(dateMomentYesterday);
+    dateArr.push(dateMoment);
+    dateArr.push(dateMomenttomorrow);
+    localStorage.setItem("vacationDates", JSON.stringify(dateArr));
+    console.log(dateArr);
     date.value = "";
         
     let ticketMasterRequestUrl = `https://app.ticketmaster.com/discovery/v2/events.json?city=${cityInput}&classificationName=${eventClassification}&startDateTime=${dateMoment}&radius=30&sort=date,name,asc&includeSpellcheck&apikey=${ticketMasterApiKey}`
@@ -78,17 +73,15 @@ let ticketMasterSearch = function (event) {
                 let eventImages = event.images[0].url; 
                 let eventUrl = event.url; 
                 let eventVenue = event._embedded.venues[0].name;
-                let id = event.id;   
+                // let eventId = event.id;   
 
-                let eventStorage = {
-                    name: eventName,
-                    id: id
-                };
+                // let eventStorage = {
+                //     name: eventName,
+                //     id: eventId
+                // };
                 
-                localStorage.setItem("eventStorage", JSON.stringify(eventStorage));           
-                
-                console.log("event: ", eventName, "image: ", eventImages,  "date: ", eventDate, "time: ", eventTime,  "event",  "link: ", eventUrl, "venue: ", eventVenue, "id: ", id);
-                
+                // localStorage.setItem("eventStorage", JSON.stringify(eventStorage));           
+                           
                 //get the UI element that the ticketMaster info will go into
                 let ticketMasterDiv = document.createElement('div');
                 ticketMasterDiv.setAttribute("style", "border: 1px solid black");
@@ -101,41 +94,42 @@ let ticketMasterSearch = function (event) {
                                 
                 let dateEl = document.createElement('span');  
                 dateEl.textContent = eventDate;
-                
                 let timeEl = document.createElement('p'); 
-                timeEl.textContent = " Start Time: " + eventTime;    
-                
-                // let pleaseNoteEl = document.createElement('p');
-                // let pleaseNote = event.pleaseNote;
-                // pleaseNoteEl.textContent = pleaseNote;               
+                timeEl.textContent = " Start Time: " + eventTime;                 
 
                 let venueEl = document.createElement('p');
                 venueEl.textContent = "Held at the "  + eventVenue;
 
                 let urlEl = document.createElement('a');
+                urlEl.setAttribute("target", "_blank");
                 let text = document.createTextNode("  Click here for more information and tickets");
                 urlEl.append(text);
                 urlEl.href = (eventUrl);
-
 
                 let imagesEl = document.createElement('img');
                 imagesEl.setAttribute("src", eventImages);
                 imagesEl.setAttribute("style", "width: 50%; height: 50%;");
                 imagesEl.alt = "photo of music act";
 
+                let iconL = document.createElement("i");
+                iconL.setAttribute("class", "fa fas-save");
+                iconL.setAttribute("id", "search-yesterday");
+                console.log(iconL);
+
                 ticketMasterDiv.append(eventTitle, dateEl, timeEl, urlEl, venueEl, imagesEl);
-                ticketmasterEl.append(ticketMasterDiv);
+                ticketmasterEl.append(ticketMasterDiv, iconL);
                 }
         }) 
 
 }
 
-
-
-
+//this causes an error, so not exactly right yet. need to get local storage for this as well with the dateArr to make it work
+// document.getElementById("search-yesterday").addEventListener("click", function () {
+//     ticketMasterSearch(localStorage.getItem(vacationDates));
+// })
 
 citySearchBtn.addEventListener("click", ticketMasterSearch);
-// showSavedMusic(savedMusic);
+
 
 $(document).ready(function () {
     $('.datepicker').datepicker({
@@ -145,4 +139,27 @@ $(document).ready(function () {
     $('.parallax').parallax();
 
 });
+
+// get saved music event data from localStorage
+
+// let showSavedMusic = function(savedMusicArr) {
+//     for(let i = 0; i < savedMusicArr.length; i++) {
+//         let musicGroup = savedMusicArr[i];
+//         console.log(musicGroup);
+        
+//     //create a button for each event viewed
+//     let musicGroupBtn = document.createElement("button");
+//     musicGroupBtn.setAttribute("class", "music-btn");
+//     musicGroupBtn.setAttribute("value", musicGroup);
+//     musicGroupBtn.textContent = musicGroup;
+
+//     savedViewsDiv.append(musicGroupBtn);
+
+//         musicGroupBtn.addEventListener("click", function() {
+//             console.log(this);
+//             ticketMasterSearch(this.value);
+//         })
+
+//     }
+// }
 
